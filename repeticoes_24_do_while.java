715491bdefb8b631b2129d9d68923a033239968f
Djailson1 <89245910+Djailson1@users.noreply.github.com>
2022-04-14 21:23:08 -0300

    Add files via upload

---
+++ repeticoes_24_do_while.java
@@ -0,0 +1,20 @@
+package br.com.java.exercicios.loops_de_repeticoes;
+
+import java.util.Scanner;
+
+/*Faça um programa usando a estrutura “faça enquanto” que leia a idade de
+várias pessoas. A cada laço, você deverá perguntar para o usuário se ele quer ou
+não continuar a digitar dados. No final, quando o usuário decidir parar, mostre
+na tela:
+a) Quantas idades foram digitadas
+b) Qual é a média entre as idades digitadas
+c) Quantas pessoas tem 21 anos ou mais.*/
+public class repeticoes_24_do_while {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int idade, totalId, somaId, mediaId, maior21Anos;
+        char continua;
+
+    }
+}
