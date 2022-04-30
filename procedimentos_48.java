6ef129abaccb8b69154c4a99cee402410dd5bd23
Djailson1 <89245910+Djailson1@users.noreply.github.com>
2022-04-29 23:18:03 -0300

    Add files via upload

---
+++ procedimentos_48.java
@@ -0,0 +1,24 @@
+package br.com.java.exercicios.loops_de_repeticoes;
+/*Crie um programa que tenha um procedimento Gerador() que, quando chamado,
+mostre a mensagem "Olá, Mundo!" com algum componente visual (linhas)
+Ex: Ao chamar Gerador() aparece:
++-------=======------+
+Olá, Mundo!
++-------=======------+*/
+public class procedimentos_48 {
+
+    public static void main(String[] args) {
+
+
+        gerador();
+
+
+    }
+    static void gerador() {
+        System.out.println ("+------===========-------+");
+        System.out.println ("       Olá, Mundo!        ");
+        System.out.println ("+------===========-------+");
+
+    }
+
+}
