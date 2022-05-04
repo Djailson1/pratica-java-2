94d8bc0c3ba24910d9631cb027fcda06219929f9
Djailson1 <89245910+Djailson1@users.noreply.github.com>
2022-05-04 00:28:12 -0300

    Update procedimento_55.java

--- procedimento_55.java
+++ procedimento_55.java
@@ -5,19 +5,27 @@ import java.util.Scanner;
 /*Faça um programa que tenha um procedimento chamado Contador() que recebe
 três valores como parâmetro: o início, o fim e o incremento de uma contagem. O
 programa principal deve solicitar a digitação desses valores e passá-los ao
-procedimento, que vai mostrar a contagem na tela.*/
+procedimento, que vai mostrar a contagem na tela.
+
+Ex: Para os valores de início (4), fim (20) e incremento(3) teremos
+Contador(4, 20, 3) vai mostrar na tela 4 >> 7 >> 10 >> 13 >> 16 >> 19 >> FIM*/
+
 public class procedimento_55 {
     public static void contadorFor(int i, int f, int p){
         int cont;
+        System.out.println("contagem de " + i + " até " + f + " de " + p + " em " + p);
 
         if (p == 0){
            p = 1;
        }
-        System.out.println("contagem de " + i + " até " + f + " de " + p + " em " + p);
-        if (i < f) {
+
+        if (i < f){
             cont = i;
+            if (cont == 1){
+                cont = p;
+            }
             while (cont <= f) {
-                System.out.print(cont + " ");
+                System.out.print(cont + " >> ");
                 cont += p;
 
             }
@@ -30,12 +38,13 @@ public class procedimento_55 {
             while(cont >= f) {
 
 
-                System.out.print(cont + " ");
+                System.out.print(cont + " >> ");
                 cont -= p;
 
 
             }
         }
+
         System.out.println("Fim!");
 
 
